38b781a9d52f90440aeafbf2dcdaa3b83eb75e06
CodingManoj <CodingManoj@gmail.com>
2025-03-21 20:59:44 -0400

    Installing Jenkins ~ AutoPush

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ fi
 echo "Installing Jenkins"
 wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum upgrade -y 
 # Add required dependencies for the jenkins package
 yum install fontconfig java-17-openjdk -y
